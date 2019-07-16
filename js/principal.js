7a5589d7553e17bb7644a40a2b007149807a665a
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2019-07-16 18:13:39 -0300

    Repetição
    - Uso do selector querySelectorAll
    - Uso laço/loop for

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,46 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var trPaciente = document.querySelector(".paciente");
-var tdPeso = trPaciente.querySelector(".info-peso");
-var tdAltura = trPaciente.querySelector(".info-altura");
-var tdImc = trPaciente.querySelector(".info-imc");
+//var trPaciente = document.querySelector(".paciente");
+//var tdPeso = trPaciente.querySelector(".info-peso");
+//var tdAltura = trPaciente.querySelector(".info-altura");
+//var tdImc = trPaciente.querySelector(".info-imc");
 
 //tdPeso.textContent = 2000;
 //tdAltura.textContent = 4;
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+for(var i=0; i<pacientes.length; i++) {
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var tdAltura = paciente.querySelector(".info-altura");
+  var tdImc = paciente.querySelector(".info-imc");
 
-if (peso <= 0 || peso > 1000) {
-  pesoEhValido = false;
-  tdImc.textContent = "Peso inválido!";
-  console.log("Peso inválido");
-}
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
 
-if (altura < 0 || altura > 3.00) {
-  alturaEhValida = false;
-  tdImc.textContent = "Altura inválida!";
-  console.log("Altura inválida");
-}
+  var pesoEhValido = true;
+  var alturaEhValida = true;
 
-if(pesoEhValido && alturaEhValida) {
-  var imc = (peso/(altura*altura));
-  tdImc.textContent = imc;
+  if (peso <= 0 || peso > 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+    paciente.style.color = "red";
+    paciente.classList.add("paciente-invalido");
+    console.log("Peso inválido");
+  }
+
+  if (altura < 0 || altura > 3.00) {
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+    // paciente.style.color = "red";
+    paciente.style.backgroundColor = "ligthcoral";
+    console.log("Altura inválida");
+  }
+
+  if(pesoEhValido && alturaEhValida) {
+    var imc = (peso/(altura*altura));
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
