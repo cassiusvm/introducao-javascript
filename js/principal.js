dd4f963c88deb742ee9644673c0997d7d89b7273
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2019-07-16 16:21:11 -0300

    Fim da Aula 02
    - Acessando elementos dentro de outros
    - Fazendo cálculos
    - Operadores lógicos
    - Validação

--- js/principal.js
+++ js/principal.js
@@ -1,14 +1,33 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "DESAPARECIDA Nutrição";
+titulo.textContent = "Aparecida Nutricionista";
 
 var trPaciente = document.querySelector(".paciente");
 var tdPeso = trPaciente.querySelector(".info-peso");
 var tdAltura = trPaciente.querySelector(".info-altura");
 var tdImc = trPaciente.querySelector(".info-imc");
 
+//tdPeso.textContent = 2000;
+//tdAltura.textContent = 4;
+
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 
-var imc = (peso/(altura*altura));
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-tdImc.textContent = imc;
+if (peso <= 0 || peso > 1000) {
+  pesoEhValido = false;
+  tdImc.textContent = "Peso inválido!";
+  console.log("Peso inválido");
+}
+
+if (altura < 0 || altura > 3.00) {
+  alturaEhValida = false;
+  tdImc.textContent = "Altura inválida!";
+  console.log("Altura inválida");
+}
+
+if(pesoEhValido && alturaEhValida) {
+  var imc = (peso/(altura*altura));
+  tdImc.textContent = imc;
+}
