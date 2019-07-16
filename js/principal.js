affcb3eee3f542363b88c77f8d0cda5b2154a628
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2019-07-16 09:07:40 -0300

    Atualização

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,14 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "DESAPARECIDA Nutrição";
+
+var trPaciente = document.querySelector(".paciente");
+var tdPeso = trPaciente.querySelector(".info-peso");
+var tdAltura = trPaciente.querySelector(".info-altura");
+var tdImc = trPaciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var imc = (peso/(altura*altura));
+
+tdImc.textContent = imc;
